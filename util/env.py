90cfa5d586e1ec315554843f529dcab649e9ba57
Achintya <achintya22052000@gmail.com>
2023-04-23 22:12:53 +0530

    added const url

--- util/env.py
+++ util/env.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 MONGO_DATABASE = os.environ.get("MONGO_DATABASE", "users")
-TOKEN = os.environ("TOKEN", "token")
+TOKEN = os.environ.get("TOKEN", "token")
+URL = "amizone.fly.dev:443"
