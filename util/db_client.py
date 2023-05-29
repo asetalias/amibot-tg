4e54e5f1f30b4cabe98427f9b4479d407e4999d2
Achintya <achintya22052000@gmail.com>
2023-05-29 19:51:16 +0530

    implemented the encrypt and decrypt functions

--- util/db_client.py
+++ util/db_client.py
@@ -1,13 +1,3 @@
-# import pymongo
-# from util.env import MONGO_URI, MONGO_DATABASE
-
-# client = pymongo.MongoClient(MONGO_URI)
-
-# db = client[MONGO_DATABASE]
-
-# # Collection, use profile to create and query the db
-# profile = db["profile"]
-
 import asyncio
 import motor.motor_asyncio
 from util.env import MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION
--- util/env.py
+++ util/env.py
@@ -5,6 +5,8 @@ load_dotenv(".env")
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 MONGO_DATABASE = os.environ.get("MONGO_DATABASE", "users")
+KEY = os.environ.get("KEY")
 TOKEN = os.environ.get("TOKEN", "token")
 URL = "amizone.fly.dev:443"
 MONGO_COLLECTION = "profile"
+
