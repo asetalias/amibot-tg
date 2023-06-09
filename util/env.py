69b8e94c862b61e9458d23ea2b001553c024b077
Achintya <achintya22052000@gmail.com>
2023-06-09 20:57:45 +0530

    easier deployment
    Signed-off-by: Achintya <achintya22052000@gmail.com>

--- util/env.py
+++ util/env.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+load_dotenv("app.env")
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 MONGO_DATABASE = os.environ.get("MONGO_DATABASE", "users")
